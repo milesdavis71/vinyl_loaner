550db136e6e74b264dcb15a38ee58b44f46ba2b6
milesdavis71 <OttosMops71>
2020-02-26 08:00:05 +0100

    Adatbázis megadása localhost-ra.

--- config/connect.php
+++ config/connect.php
@@ -1,8 +1,8 @@
 <?php
 
-//$con = new mysqli('localhost','root','','zene_uj','3306');
+$con = new mysqli('localhost','root','','milesdavis71','3306');
 //$con = new mysqli('db4free.net','milesdavis','ottosmops','vinylcenter','3306');
-$con = new mysqli('mysql.nethely.hu','milesdavis71','OttosMops71','milesdavis71','3306');
+//$con = new mysqli('mysql.nethely.hu','milesdavis71','OttosMops71','milesdavis71','3306');
 
 
 
